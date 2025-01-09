c19d6eeacfa73fa0f474d627d10c15e587fbf684
diego950217 <diegofernando1702@gmail.com>
2025-01-09 12:22:54 -0500

    S3 routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1931,18 +1931,6 @@ router.get('/stations/client/:client_id', async (req, res) => {
   }
 });
 
-
-// Configuración de almacenamiento con Multer para inspecciones
-const inspectionStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '..', '..', 'public', 'media', 'inspections'));
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  }
-});
-
 // File filter para imágenes
 const inspectionFileFilter = (req, file, cb) => {
   const allowedTypes = /jpeg|jpg|png|gif/;
